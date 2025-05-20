4b95a51bb2c9673a4a3180b8155a8ea9d5cfe7c9
Moisés Tchipanga <moisestchipanga32@gmail.com>
2025-05-20 21:55:49 +0100

    Exercitando Javascript

--- JavaScript/Script.js
+++ JavaScript/Script.js
@@ -1,10 +1,22 @@
-let contando = 0;
+let contando = parseInt(localStorage.getItem('contador')) ||0;
 
+let intervalo;
+
+document.getElementById("numero").textContent = contando;
+// Após carregar o valor
+if(contando > 0){
+    document.getElementById("numero").style.color = 'green';
+} else if(contando < 0){
+    document.getElementById("numero").style.color = 'red';
+} else {
+    document.getElementById("numero").style.color = 'black';
+}1
 
 document.getElementById("botao").addEventListener("click",function
 (){
     contando++;
     document.getElementById("numero").textContent = contando;
+    localStorage.setItem('contador', contando);
     if(contando === 0){
         document.getElementById("numero").style.color = 'black';
     } else if(contando > 0){
@@ -23,17 +35,43 @@ document.getElementById("botao2"). addEventListener("click", function
         document.getElementById("numero").style.color = 'black';
     } 
     document.getElementById("numero").textContent = contando;
+    localStorage.setItem('contador', contando);
 });
 
 document.getElementById("botao3").addEventListener("click", function() {
 
         contando--;
         document.getElementById("numero").textContent = contando;
+        localStorage.setItem('contador', contando);
         if(contando === 0){
         document.getElementById("numero").style.color = 'black';
         }else if(contando < 0){
             document.getElementById("numero").style.color = 'red';
         }
+});
+
+// Função para atualizar tudo
+function atualizarContador() {
+    document.getElementById("numero").textContent = contando;
+    localStorage.setItem('contador', contando);
+}
+
+atualizarContador();
+
+// Botão Auto
+document.getElementById("botao4").addEventListener("click", function() {
+    if(!intervalo) {
+        intervalo = setInterval(() => {
+            contando++;
+            atualizarContador();
+        }, 1000);
+        this.textContent = "Parar Auto";
+    } else {
+        clearInterval(intervalo);
+        intervalo = null;
+        this.textContent = "Iniciar Auto";
+    }
+    this.classList.toggle('ativo');
 });
 
 
